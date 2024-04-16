53bce4e6110bcf6680c8fcc162247205eb40f9f3
Nursultan Kenzhebaev <nursultan@beeworx.de>
2024-04-16 15:39:14 +0600

    Add Button component to render a customizable button

---
+++ src/components/UI/Button/Button.jsx
@@ -0,0 +1,11 @@
+import PropTypes from "prop-types";
+
+const Button = ({ title }) => {
+  return <button>{title}</button>;
+};
+
+Button.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default Button;
