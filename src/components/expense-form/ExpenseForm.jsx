69a2ba7bd73f7f9a5ff53fefda0ee29e065821f0
Nursultan Kenzhebaev <nursultan@beeworx.de>
2024-04-16 16:50:11 +0600

    Add onClick event to Button component in Button.jsx and ExpenseForm.jsx

--- src/components/expense-form/ExpenseForm.jsx
+++ src/components/expense-form/ExpenseForm.jsx
@@ -2,7 +2,9 @@ import Button from "../UI/Button/Button";
 import { FormInput } from "../UI/FormInput/FormInput";
 import css from "./ExpenseForm.module.css";
 
-export const ExpenseForm = () => {
+import PropTypes from "prop-types";
+
+export const ExpenseForm = ({ onShowForm }) => {
   return (
     <form>
       <div className={css.formInputContainer}>
@@ -11,9 +13,13 @@ export const ExpenseForm = () => {
       </div>
       <FormInput labelName="Датировать" id="date" type="date" />
       <div className={css.formInputFooter}>
-        <Button title="Отмена" styles={css.buttonSpace} />
+        <Button title="Отмена" styles={css.buttonSpace} onClick={onShowForm} />
         <Button title="Добавить расход" />
       </div>
     </form>
   );
+};
+
+ExpenseForm.propTypes = {
+  onShowForm: PropTypes.func.isRequired,
 };
